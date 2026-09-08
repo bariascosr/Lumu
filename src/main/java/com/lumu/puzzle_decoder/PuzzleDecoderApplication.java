19a562550edf79dd4bd2f02c54bde1364e9405ce
bariascosr <bariascosr@unal.edu.co>
2026-09-08 12:52:18 -0500

    adding first files pom.xml and basic model

---
+++ src/main/java/com/lumu/puzzle_decoder/PuzzleDecoderApplication.java
@@ -0,0 +1,13 @@
+package com.lumu.puzzle_decoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuzzleDecoderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuzzleDecoderApplication.class, args);
+	}
+
+}
