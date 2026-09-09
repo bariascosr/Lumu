c1eafc1fe2328d221bd8d4bf2aa315da2dce64ad
bariascosr <bariascosr@unal.edu.co>
2026-09-09 12:21:41 -0500

    adding service logic with other needed classes

--- src/main/java/com/lumu/puzzle_decoder/client/PuzzleServerClient.java
+++ src/main/java/com/lumu/puzzle_decoder/client/PuzzleServerClient.java
@@ -8,7 +8,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface PuzzleServerClient {
-    
+
     Mono<PuzzleFragment> fetchFragment(int id);
 
     Flux<PuzzleFragment> fetchFragments(List<Integer> ids);
